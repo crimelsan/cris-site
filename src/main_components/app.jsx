61710e197fb119d8cb7ee4e4b71b38c92d3ab262
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-24 18:57:36 -0500

    Fleshed out project section a bit, adding projects in

--- src/main_components/app.jsx
+++ src/main_components/app.jsx
@@ -1,8 +1,8 @@
-import React from "react"
 import Head from "./components/header.jsx"
 import Navbar from "./components/navbar.jsx"
 import About from "./components/about.jsx"
 import Projects from "./components/projects.jsx"
+
 
 export default function App() {
     return (
