e544bef301787f87055f346c33a4c9b6b506e951
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-07-29 18:05:21 -0500

    Added backend!

--- src/blog_components/app.jsx
+++ src/blog_components/app.jsx
@@ -3,10 +3,20 @@ import Navbar from "../main_components/components/navbar.jsx"
 
 export default function App() {
     const [tabState, setTabState] = useState("blog")
+    const [showBlog, setShowBlog] = useState(false)
 
     return (
-        <>     
-            <Navbar/>       
-        </>
+        <body onAnimationEnd={() => setShowBlog(true)}>     
+            <Navbar/>  
+            {showBlog && <div className="fade">
+                <div className="header">
+                    <h1>Cris's Blog Space</h1>
+                    <p>
+                        Hi! This is where I post all of my blogs. I'm planning on posting different types of blogs for how I currently feel. Blogs could maybe range from me talking about
+                        music or some spot I like or just about anything, so click on something below and start reading!
+                    </p>
+                </div>
+            </div>}     
+        </body>
     )
 }
--- src/main_components/components/header.jsx
+++ src/main_components/components/header.jsx
@@ -3,16 +3,16 @@ export default function Head() {
         <div style={{position: 'relative'}}>
             <div className="gallery-container">
                 <div className="overlay"/> 
-                <img id="one" src="/assets/images/img1.png"/>
-                <img id="two"src="/assets/images/img2.png"/>
-                <img id="three"src="/assets/images/img3.png"/>
-                <img id="four"src="/assets/images/img4.png"/>
-                <img id="five"src="/assets/images/img5.png"/>
-                <img id="six"src="/assets/images/img6.png"/>
-                <img id="seven"src="/assets/images/img7.png"/>
-                <img id="eight"src="/assets/images/img8.png"/>
-                <img id="nine"src="/assets/images/img9.png"/>
-                <img id="ten"src="/assets/images/img10.png"/>
+                    <img id="one" src="/assets/images/img1.png"/>
+                    <img id="two"src="/assets/images/img2.png"/>
+                    <img id="three"src="/assets/images/img3.png"/>
+                    <img id="four"src="/assets/images/img4.png"/>
+                    <img id="five"src="/assets/images/img5.png"/>
+                    <img id="six"src="/assets/images/img6.png"/>
+                    <img id="seven"src="/assets/images/img7.png"/>
+                    <img id="eight"src="/assets/images/img8.png"/>
+                    <img id="nine"src="/assets/images/img9.png"/>
+                    <img id="ten"src="/assets/images/img10.png"/>
                 <div className="head-text">
                     <h1>{"Welcome to Cris's personal website >:D"}</h1>
                     <span>he does things apparently</span>
