f0b12d30b40375ec320605f874935b48654f3dfc
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-08-06 19:20:19 -0500

    Finished music portion of site

--- src/music_components/components/body.jsx
+++ src/music_components/components/body.jsx
@@ -3,37 +3,73 @@ import MusicItem from "./music_item.jsx"
 
 export default function Body() {
     const [data, setData] = useState([])
+    const [ genreFilter, setGenreFilter] = useState("All")
+    const [ typeFilter, setTypeFilter] = useState("All")
     useEffect(() => {
-        fetch('http://localhost:8008/blogdata')
+        fetch('http://localhost:8008/musicdata')
         .then(res => res.json())
         .then(data => setData(data))
         .catch(err => console.log(err))   
     }, [])
+
+    const genres = []
+    const types = []
+
+    data.forEach(d => {
+        if (!genres.includes(d.genre)) {
+            genres.push(d.genre)
+        }
+
+        if (!types.includes(d.type)) {
+            types.push(d.type)
+        }
+    })
+
+    const filteredData = data.filter(d => ((typeFilter === "All" || d.type === typeFilter) && (genreFilter === "All" || d.genre === genreFilter)))
+
+    function handleGenreFilter(genre) {
+        setGenreFilter(genre)
+    }
+
+    function handleTypeFilter(type) {
+        setTypeFilter(type)
+    }
 
     return(
         <div className="body-container">
             <div className="filter-container">
                 <div>
                     <label htmlFor="type">Type: </label>
-                    <select id="type">
+                    <select id="type" onChange={(e) => handleTypeFilter(e.target.value)}>
                         <option value="All">All</option>
-                        <option value="Original">Original</option>      
-                        <option value="Remix">Remix</option>                
+                        {types.map((type, i) => (
+                            <option key={i} value={type}>{type}</option>
+                        ))}           
                     </select>                    
                 </div>
                 <div>
                     <label htmlFor="genre">Genre: </label>
-                    <select id="genre">
+                    <select id="genre" onChange={(e) => handleGenreFilter(e.target.value)}>
                         <option value="All">All</option>
-                        <option value="Jazz">Jazz</option>
-                        <option value="8-Bit">8-Bit</option>
-                        <option value="Electronic">Electronic</option>
+                        {genres.map((genre, i) => (
+                            <option key={i} value={genre}>{genre}</option>
+                        ))}      
                     </select>   
                 </div>
             </div>
             <div className="body-content">
-                <MusicItem/>
-                <MusicItem/>
+                {filteredData.map((d, i) => (
+                    <MusicItem 
+                        key={i} 
+                        ID={d.id} 
+                        title={d.title} 
+                        genre={d.genre} 
+                        musicDir={d.music_dir} 
+                        imgDir={d.img_dir} 
+                        description={d.descripton} 
+                        type={d.type} 
+                    />
+                ))}
             </div>
         </div>
     )
--- src/music_components/components/music_item.jsx
+++ src/music_components/components/music_item.jsx
@@ -1,18 +1,94 @@
-
+import { useState, useRef, useEffect } from "react"
 
 export default function MusicItem(props) {
-    return (
-        <div className="music-content">
-            <div>
-                <img/>
-                <span></span>
-                <div className="audio-control">
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [currentTime, setCurrentTime] = useState(0)
+    const [duration, setDuration] = useState(0)
+    const [displayDescription, setDisplayDescription] = useState(false)
 
+    const audioRef = useRef(null)
+
+    function showDescription() {
+        setDisplayDescription(true)
+    }
+
+    function hideDescription() {
+        setDisplayDescription(false)
+    }
+
+    function handleSeek(e) {
+        audioRef.current.currentTime = e.target.value;
+        setCurrentTime(e.target.value)
+    }
+
+    function handleTimeUpdate() {
+        setCurrentTime(audioRef.current.currentTime)
+        setDuration(audioRef.current.duration)
+    }
+
+    function handlePause() {
+        audioRef.current.pause()
+        setIsPlaying(false)
+    }
+
+    function handlePlay() {
+        audioRef.current.play();
+        setIsPlaying(true)
+    }
+
+    function handlePausePlay() {
+        if (isPlaying) {
+            handlePause()
+        } else {
+            handlePlay()
+        }
+    }
+
+    function formatTime(time) {
+        const minutes = Math.floor(time / 60)
+        const seconds = Math.floor(time % 60)
+        const formattedseconds = seconds.toString().padStart(2, "0");
+        return `${minutes}:${formattedseconds}`
+    }
+
+    useEffect(() => {
+        const currentVal = audioRef.current
+        currentVal.addEventListener("timeupdate", handleTimeUpdate);
+        return () => {
+            currentVal.removeEventListener("timeupdate", handleTimeUpdate)
+        }
+    }, [])
+
+    return (
+        <div className="music-content" onMouseEnter={showDescription} onMouseLeave={hideDescription}>
+            <div className="main-content">
+                <img className="music-cover" src={`/assets/images/music_imgs/${props.imgDir}`}/>
+                <div className="music-text">
+                    <span>Title: {props.title}</span>
+                    <span>Song-Type: {props.type}</span>
+                    <span>Genre: {props.genre}</span>
+                </div>
+                <div className="audio-control">
+                    <input
+                        type="range"
+                        min="0"
+                        max={duration}
+                        value={currentTime}
+                        onChange={handleSeek}
+                    />
+                    <audio ref={audioRef} src={`/assets/audio/music/${props.musicDir}`}/>
+                    <div className="duration-tracker">
+                        <p>{formatTime(currentTime)}</p>
+                        <p>{formatTime(duration)}</p>
+                    </div>
+                    <button onClick={handlePausePlay}>
+                        <img src={`assets/images/${isPlaying ? "pause" : "play"}.png`}/>
+                    </button>
                 </div>
             </div>
-            <p>
-                
-            </p>
+            {displayDescription && <p className="description">
+                {props.description}
+            </p>}
         </div>
     )
 }
