18e1205e2d82159d9e65749e054c3087490e3998
RandomDude22 <32312676+crimelsan@users.noreply.github.com>
2025-08-18 12:37:51 -0500

    Idk

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        intro: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'main.html'), 
+        blog: resolve(__dirname, 'blog.html'),
+        music: resolve(__dirname, 'music.html'),
+        gallery: resolve(__dirname, 'blog.html')
+      },
+    },
+  },
+});
+
